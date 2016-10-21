066d4e94d25e0f67096276b9ad8fecc5a40bc3da
MaxAhnfeldt <maximilian.ahnfeldt@nordakademie.de>
2016-10-21 11:39:20 +0200

    Check this out

---
+++ src/uebung_2_2/uebung_2_2.java
@@ -0,0 +1,22 @@
+package uebung_2_2;
+
+public class uebung_2_2 {
+
+	public static void main(String[] args) {
+
+		double a=8;
+		double b=3;
+		
+		double klammer=a+b;
+		double term1=a*a;
+		double term2=2*a*b;
+		double term3=b*b;
+		
+		double loesung1=klammer*klammer;
+		double loesung2=term1+term2+term3;
+		
+		System.out.println("x1="+loesung1);
+		System.out.println("x2="+loesung2);
+	}
+
+}
